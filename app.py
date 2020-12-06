c2bf973069fcba08a5fb7efd66f938461c3740a4
Nur988 <46999128+Nur988@users.noreply.github.com>
2020-12-07 00:29:23 +0600

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,3 @@ if file_up is not None:
     image = Image.open(file_up)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
-    st.write("Just a second...")
-    labels = predict(file_up)
-
-    # print out the top 5 prediction labels with scores
-    for i in labels:
-        st.write("Prediction (index, name)", i[0], ",   Score: ", i[1])
